609fac762be216850fa526884a14e0df036cc9f1
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-13 09:59:50 +0600

    Passing parameter in arrow function on buttonclick

--- src/mycomponents/Arrow.js
+++ src/mycomponents/Arrow.js
@@ -2,12 +2,16 @@ import React from 'react'
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
+const AlertME = (a)=>{
+
+    alert(a);
+}
 
 const Arrow = ()=>{
 
     return(
 
-        <button className = "btn btn-danger">Arrow Test Button</button>
+        <button className = "btn btn-danger" onClick={AlertME.bind(this,"Test Complete")}>Arrow Test Button</button>
     );
 }
 
