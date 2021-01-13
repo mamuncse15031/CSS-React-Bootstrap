e2bbeb6c981080515bae407ff029fbd188286260
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-13 09:58:16 +0600

    Arrow Function button click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Welcome from './mycomponents/Welcome'
+import Arrow from './mycomponents/Arrow'
 
 function App() {
   return (
     <div className="App">
 
       <Welcome></Welcome>
+      <Arrow></Arrow>
       
     </div>
   );
---
+++ src/mycomponents/Arrow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+
+
+const Arrow = ()=>{
+
+    return(
+
+        <button className = "btn btn-danger">Arrow Test Button</button>
+    );
+}
+
+export default Arrow;
