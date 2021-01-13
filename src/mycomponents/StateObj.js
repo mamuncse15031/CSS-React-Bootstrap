17301eb418018a095426c32d8230f0904c63c05a
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-13 22:11:21 +0600

    state keyword using constructor

--- src/mycomponents/StateObj.js
+++ src/mycomponents/StateObj.js
@@ -2,12 +2,28 @@ import React, {Component} from 'react'
 
 class StateObj extends Component{
 
+    constructor(){
+        super()
+
+        this.state={
+            name: "Mamun",
+            age: "28 years",
+            height: "5 feet 11 inches"
+
+        }
+    }
+
 
 
     render(){
 
-        return <h1>State Object</h1>
-    }
+        return <div>
+                <h1>{this.state.name}</h1>
+                <h1>{this.state.age}</h1>
+                <h1>{this.state.height}</h1>
+                
+            </div>
+            }
 }
 
 export default StateObj;
