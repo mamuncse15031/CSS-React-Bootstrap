6122b9ad07291cceb17af242724b0d6d90ec0536
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-15 00:03:50 +0600

    Refresh Using force Update method

--- src/mycomponents/Refresh.js
+++ src/mycomponents/Refresh.js
@@ -1,14 +1,24 @@
 import React, {Component} from 'react'
 
 class Refresh extends Component{
+    
     constructor(){
-        super()
-        
+        super();
+
+        this.RefreshNow = this.RefreshNow.bind(this);
+    }
+    RefreshNow(){
+        this.forceUpdate();
     }
 
     
     render(){
-        return
+        return(
+            <div>
+                <button class="btn btn-primary" onClick={this.RefreshNow}>Refresh Button</button>
+                <h1>{Math.random()}</h1>
+            </div>
+        )
     }
 }
 export default Refresh;
