36d9bd8e7da14d828e93e24239e2ebe6468fed4d
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-14 23:01:12 +0600

    general conditional return

--- src/mycomponents/Condition1Class.js
+++ src/mycomponents/Condition1Class.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react'
 
-class Condition1Class{
+class Condition1Class extends Component{
+    constructor(){
+        super()
+        this.state={
+            login: false
+        }
+    }
 
     
     render(){
-        return
+        if(this.state.login==true){
+            return (<button class="btn btn-primary">Logout</button>)
+        }
+        else{
+            return (<button class="btn btn-primary">Login</button>)
+        }
     }
 }
 export default Condition1Class;
