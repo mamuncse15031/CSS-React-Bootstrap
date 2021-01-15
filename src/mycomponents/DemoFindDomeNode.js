09ea82ef8406f1b327e3f34a90ec9a9df67efb1a
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-15 19:13:35 +0600

    Create class component for finddomnode method

---
+++ src/mycomponents/DemoFindDomeNode.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DemoFindDomeNode extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default DemoFindDomeNode;
