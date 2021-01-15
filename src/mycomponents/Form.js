7c028fdeb33683a2867d1800aa4dd4b12017716e
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-15 22:18:14 +0600

    onChange method in single input

--- src/mycomponents/Form.js
+++ src/mycomponents/Form.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+
+    constructor(){
+        super()
+
+        this.state = {
+            name: " "
+        }
+    }
+
+    onChangeHandler=(event)=>{
+
+        var newName = event.target.value
+        this.setState({name:newName})
+
+
+    }
+
+
+
     render() {
         return (
             <div>
                 <h3>My First Form</h3>
-                <input type="text" placeholder="Enter Your Name"></input>
+                <input onChange={this.onChangeHandler} type="text" placeholder="Enter Your Name"></input>
                 <br></br>
                 <input type="submit" value="Submit"></input>
+                <p>{this.state.name}</p>
             </div>
         );
     }
