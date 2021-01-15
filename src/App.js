1b522ff39a7c197576a052d2aca919b03248f4c1
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-15 19:57:21 +0600

    New demo class created for findDOMNode method and an image is shown

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
       <NewDemoHydrate></NewDemoHydrate> */}
       <DemoFindDomeNode></DemoFindDomeNode>
       
+      
     </div>
   );
 }
---
+++ src/mycomponents/DemoFindDomeNode.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class DemoFindDomeNode extends Component {
+
+    render() {
+        return (
+            <div>
+                <button class='btn btn-primary'>Change Image</button><br></br>
+                <img src = "https://rabbil.com/storage/blogpost.jpg"></img>
+            </div>
+        );
+    }
+}
+
+export default DemoFindDomeNode;
