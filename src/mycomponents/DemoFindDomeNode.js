c4ec3ee6648cae23c90da3e4a14607d395d2da9f
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-15 20:08:52 +0600

    findDomeNode done

--- src/mycomponents/DemoFindDomeNode.js
+++ src/mycomponents/DemoFindDomeNode.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 
 
 class DemoFindDomeNode extends Component {
 
+    changeImage(){
+
+
+        var imgID = document.getElementById("imgID");
+        ReactDOM.findDOMNode(imgID).src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+
+    }
+
     render() {
         return (
             <div>
-                <button class='btn btn-primary'>Change Image</button><br></br>
-                <img src = "https://rabbil.com/storage/blogpost.jpg"></img>
+                <button onClick = {this.changeImage}>Change Image</button><br></br>
+                <img src = "https://rabbil.com/storage/blogpost.jpg" id="imgID"></img>
             </div>
         );
     }
