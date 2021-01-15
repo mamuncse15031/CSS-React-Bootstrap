16bbd888ea64284f01f9bba7acb2d07bdab63694
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-15 19:00:08 +0600

    render method without callback

--- src/mycomponents/NewDemoRender.js
+++ src/mycomponents/NewDemoRender.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import ReactDOM  from 'react-dom'
 
 class NewDemoRender extends Component {
+
+
+    change(){
+        var container = document.getElementById("myid")
+        var element = <h1>My name is Shuvo, I am from React Dom</h1>
+        ReactDOM.render(element, container);
+    }
+
     render() {
         return (
             <div>
-                <button>Change</button>
-                <h1>My Name is Shuvo</h1>
+                <button class='btn btn-primary' onClick={this.change}>Change</button>
+                <h1 id='myid'>My Name is Shuvo</h1>
             </div>
         );
     }
