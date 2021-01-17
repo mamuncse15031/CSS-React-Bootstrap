e0b80e3661ed03f2b931c7a32171cb0b691226ec
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-17 22:11:20 +0600

    General Select Option created

--- src/mycomponents/Select.js
+++ src/mycomponents/Select.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class Select extends Component {
+
+    constructor(){
+        super()
+
+        this.state={
+            city1: 'Dhaka',
+            city2: 'Rangpr',
+            city3: 'Rajshahi',
+            city4: 'Khulna'
+        }
+    }
     render() {
         return (
             <div>
-                
+                <form>
+                <Select value="City">
+                    <option>{this.state.city1}</option>
+                    <option>{this.state.city2}</option>
+                    <option>{this.state.city3}</option>
+                    <option>{this.state.city4}</option>
+                </Select>
+                </form>
             </div>
         );
     }
