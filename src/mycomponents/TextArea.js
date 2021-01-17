ec95bf5611fe4b0393271385ac50f2ecb29bbd94
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-17 21:57:16 +0600

    Text Area completed with user input

--- src/mycomponents/TextArea.js
+++ src/mycomponents/TextArea.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class TextArea extends Component {
+
+    constructor(){
+        super()
+
+        this.state={
+            userInput: " "
+        }   
+
+    }
+    onChangeHandler=(event)=>{
+        var myText = event.target.value;
+        this.setState({userInput:myText})
+    }
+    
     render() {
         return (
             <div>
-                <textarea value="My Text"></textarea>
+                <textarea placeholder="Insert Your Text" onChange={this.onChangeHandler}></textarea>
+                <p>{this.state.userInput}</p>
             </div>
         );
     }
