b2fe9157906078367a0f6cbbc88feb5f18739ace
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-18 08:50:02 +0600

    General List created

--- src/mycomponents/List.js
+++ src/mycomponents/List.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class List extends Component {
+
+
+    itemData=(a)=>{
+        return <li>{a}</li>
+    }
+
     render() {
+
+        const myList = ['a','b','c','d','e'];
+        const listItem = myList.map(this.itemData)
         return (
             <div>
-                
+                <ol>
+                    {listItem}
+                </ol>
             </div>
         );
     }
