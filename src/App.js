f5c2118923e558b2d9c7fe05329d57ae6a0971c2
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-18 09:20:43 +0600

    JSON Array created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import './App.css';
 import JsonLinst from './mycomponents/JsonLinst';
 import List from './mycomponents/List';
 import Select from './mycomponents/Select';
+/*import Welcome from './mycomponents/Welcome'
+import Arrow from './mycomponents/Arrow'
+import StateObj from './mycomponents/StateObjClass'
+import SetStateClassCompo from './mycomponents/SetStateClassCompo';
+import Condition1Class from './mycomponents/Condition1Class'
+import Condition2ElementClass from './mycomponents/Condition2ElementClass'
+import Refresh from './mycomponents/Refresh'
+import NewDemoRender from './mycomponents/NewDemoRender'
+import NewDemoHydrate from './mycomponents/NewDemoHydrate'
+import DemoFindDomeNode from './mycomponents/DemoFindDomeNode'
+import Form from './mycomponents/Form'*/
 import SignupForm from './mycomponents/SignupForm'
 import TextArea from './mycomponents/TextArea';
 
@@ -9,6 +20,19 @@ import TextArea from './mycomponents/TextArea';
 function App() {
   return (
     <div className="App">
+
+      {/*<Welcome></Welcome>
+      <Arrow></Arrow>
+      <StateObj></StateObj>
+      <SetStateClassCompo></SetStateClassCompo>
+      <Condition1Class></Condition1Class>
+      <Condition2ElementClass></Condition2ElementClass>
+      <Refresh></Refresh>
+      <NewDemoRender></NewDemoRender>
+      <NewDemoHydrate></NewDemoHydrate> 
+      <DemoFindDomeNode></DemoFindDomeNode>
+
+      <Form></Form>*/} 
 
       <SignupForm></SignupForm>
       <TextArea></TextArea>
--- src/mycomponents/JsonLinst.js
+++ src/mycomponents/JsonLinst.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
 class JsonLinst extends Component {
+
+    dataItem=(data)=>{
+        return <option>{data}</option>
+    }
+
+
     render() {
+        const list=[
+            {
+                city: 'Dhaka',
+                zip: 1700
+            },
+            {
+                city: 'Khulna',
+                zip: 1800
+            },
+            {
+                city: 'Tangail',
+                zip: 1900
+            }
+        ]
+        const listItem = list.map(this.dataItem)
+
+
         return (
             <div>
-                
+                <select>
+                    {listItem}
+                </select>
             </div>
         );
     }
