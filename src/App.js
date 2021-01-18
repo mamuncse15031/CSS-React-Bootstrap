5c56f2641dc9f0224754790cb09b9f690e595033
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-18 09:07:49 +0600

    Created Json List Class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import './App.css';
+import JsonLinst from './mycomponents/JsonLinst';
 import List from './mycomponents/List';
 import Select from './mycomponents/Select';
-/*import Welcome from './mycomponents/Welcome'
-import Arrow from './mycomponents/Arrow'
-import StateObj from './mycomponents/StateObjClass'
-import SetStateClassCompo from './mycomponents/SetStateClassCompo';
-import Condition1Class from './mycomponents/Condition1Class'
-import Condition2ElementClass from './mycomponents/Condition2ElementClass'
-import Refresh from './mycomponents/Refresh'
-import NewDemoRender from './mycomponents/NewDemoRender'
-import NewDemoHydrate from './mycomponents/NewDemoHydrate'
-import DemoFindDomeNode from './mycomponents/DemoFindDomeNode'
-import Form from './mycomponents/Form'*/
 import SignupForm from './mycomponents/SignupForm'
 import TextArea from './mycomponents/TextArea';
 
@@ -20,23 +10,11 @@ function App() {
   return (
     <div className="App">
 
-      {/*<Welcome></Welcome>
-      <Arrow></Arrow>
-      <StateObj></StateObj>
-      <SetStateClassCompo></SetStateClassCompo>
-      <Condition1Class></Condition1Class>
-      <Condition2ElementClass></Condition2ElementClass>
-      <Refresh></Refresh>
-      <NewDemoRender></NewDemoRender>
-      <NewDemoHydrate></NewDemoHydrate> 
-      <DemoFindDomeNode></DemoFindDomeNode>
-
-      <Form></Form>*/} 
-
       <SignupForm></SignupForm>
       <TextArea></TextArea>
       <Select></Select>
       <List></List>
+      <JsonLinst></JsonLinst>
 
       
       
